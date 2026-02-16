2db8307515ffd956bac505ac21ca94a4456fe811
zaneschepke <dev@zaneschepke.com>
2026-02-16 07:54:35 -0500

    refactor: simplify single app enforce

--- composeApp/src/jvmMain/kotlin/com/zaneschepke/wireguardautotunnel/desktop/Main.kt
+++ composeApp/src/jvmMain/kotlin/com/zaneschepke/wireguardautotunnel/desktop/Main.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
@@ -31,6 +32,7 @@ import com.dokar.sonner.Toaster
 import com.dokar.sonner.rememberToasterState
 import com.kdroid.composetray.tray.api.ExperimentalTrayAppApi
 import com.kdroid.composetray.tray.api.Tray
+import com.kdroid.composetray.utils.SingleInstanceManager
 import com.kdroid.composetray.utils.isMenuBarInDarkMode
 import com.zaneschepke.wireguardautotunnel.client.di.databaseModule
 import com.zaneschepke.wireguardautotunnel.client.di.serviceModule
@@ -42,9 +44,7 @@ import com.zaneschepke.wireguardautotunnel.desktop.di.viewModelModule
 import com.zaneschepke.wireguardautotunnel.desktop.ui.common.bar.TitleBar
 import com.zaneschepke.wireguardautotunnel.desktop.ui.theme.WGTunnelTheme
 import com.zaneschepke.wireguardautotunnel.desktop.viewmodel.AppViewModel
-import java.io.File
-import java.io.RandomAccessFile
-import java.nio.channels.FileLock
+import java.nio.file.Paths
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
@@ -54,139 +54,114 @@ import org.koin.dsl.koinConfiguration
 import org.orbitmvi.orbit.compose.collectAsState
 
 @OptIn(ExperimentalTrayAppApi::class)
-fun main() {
+fun main() = application {
+    var isWindowVisible by remember { mutableStateOf(true) }
+    SingleInstanceManager.configuration =
+        SingleInstanceManager.Configuration(
+            lockFilesDir = Paths.get(FilePathsHelper.getDatabaseDir().path),
+            lockIdentifier = "wg_tunnel",
+        )
+    val isSingleInstance =
+        SingleInstanceManager.isSingleInstance(onRestoreRequest = { isWindowVisible = true })
 
-    val appDir = FilePathsHelper.getDatabaseDir()
-    if (!appDir.exists()) {
-        appDir.mkdirs()
+    if (!isSingleInstance) {
+        exitApplication()
+        return@application
     }
 
-    val lockFile = File(appDir, "wgtunnel.lock")
+    KoinApplication(
+        configuration =
+            koinConfiguration(
+                declaration = { modules(databaseModule, serviceModule, viewModelModule) }
+            )
+    ) {
+        val appIcon = painterResource(Res.drawable.wgtunnel)
+        val appName = stringResource(Res.string.app_name)
 
-    // enforce single instance
-    try {
-        val raf = RandomAccessFile(lockFile, "rw")
-        val lock: FileLock? = raf.channel.tryLock()
+        val isMenuBarDark = isMenuBarInDarkMode()
+        val windowState = rememberWindowState(size = DpSize(1200.dp, 800.dp))
+        val toaster = rememberToasterState()
 
-        if (lock == null) {
-            Logger.i { "App is already running." }
-            return
+        Tray(
+            iconContent = {
+                Icon(
+                    imageVector = vectorResource(Res.drawable.wgtunnel),
+                    contentDescription = appName,
+                    tint = if (isMenuBarDark) Color.White else Color.Black,
+                )
+            },
+            tooltip = appName,
+        ) {
+            if (!isWindowVisible) {
+                Item(label = "Open") {
+                    Logger.i { "Open menu item clicked" }
+                    isWindowVisible = true
+                }
+            }
+            Item(label = "Exit") { exitApplication() }
         }
 
-        Runtime.getRuntime()
-            .addShutdownHook(
-                Thread {
-                    lock.release()
-                    raf.close()
-                }
-            )
+        Window(
+            visible = isWindowVisible,
+            onCloseRequest = {
+                isWindowVisible = false
+                Logger.i { "OS close requested - hiding to tray" }
+            },
+            title = stringResource(Res.string.app_name),
+            icon = appIcon,
+            state = windowState,
+            undecorated = true,
+            transparent = true,
+        ) {
+            window.minimumSize = java.awt.Dimension(800, 650)
 
-        application {
-            KoinApplication(
-                configuration =
-                    koinConfiguration(
-                        declaration = { modules(databaseModule, serviceModule, viewModelModule) }
-                    )
-            ) {
-                val isWindowVisible = remember { mutableStateOf(true) }
+            val viewModel: AppViewModel = koinViewModel()
+            val uiState by viewModel.collectAsState()
 
-                val appIcon = painterResource(Res.drawable.wgtunnel)
-                val appName = stringResource(Res.string.app_name)
-
-                val isMenuBarDark = isMenuBarInDarkMode()
-                val windowState = rememberWindowState(size = DpSize(1200.dp, 800.dp))
-                val toaster = rememberToasterState()
-
-                Tray(
-                    iconContent = {
-                        Icon(
-                            imageVector = vectorResource(Res.drawable.wgtunnel),
-                            contentDescription = appName,
-                            tint = if (isMenuBarDark) Color.White else Color.Black,
-                        )
-                    },
-                    primaryAction = {
-                        Logger.i { "Tray primary action triggered" }
-                        isWindowVisible.value = true
-                    },
-                    tooltip = appName,
+            WGTunnelTheme(uiState.theme) {
+                Surface(
+                    modifier =
+                        Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Item(label = "Open") {
-                        Logger.i { "Open menu item clicked" }
-                        isWindowVisible.value = true
-                    }
-                    Item(label = "Exit") { exitApplication() }
-                }
-
-                Window(
-                    visible = isWindowVisible.value,
-                    onCloseRequest = {
-                        isWindowVisible.value = false
-                        Logger.i { "OS close requested - hiding to tray" }
-                    },
-                    title = stringResource(Res.string.app_name),
-                    icon = appIcon,
-                    state = windowState,
-                    undecorated = true,
-                    transparent = true,
-                ) {
-                    window.minimumSize = java.awt.Dimension(800, 650)
-
-                    val viewModel: AppViewModel = koinViewModel()
-                    val uiState by viewModel.collectAsState()
-
-                    WGTunnelTheme(uiState.theme) {
-                        Surface(
-                            modifier =
-                                Modifier.fillMaxSize()
-                                    .background(MaterialTheme.colorScheme.background),
-                            color = MaterialTheme.colorScheme.background,
-                        ) {
-                            Column {
-                                TitleBar(onClose = { isWindowVisible.value = false })
-                                App(uiState, viewModel, toaster)
-                                Toaster(
-                                    state = toaster,
-                                    elevation = 0.dp,
-                                    border = { BorderStroke(0.dp, Color.Transparent) },
-                                    background = {
-                                        SolidColor(MaterialTheme.colorScheme.inverseOnSurface)
+                    Column {
+                        TitleBar(onClose = { isWindowVisible = false })
+                        App(uiState, viewModel, toaster)
+                        Toaster(
+                            state = toaster,
+                            elevation = 0.dp,
+                            border = { BorderStroke(0.dp, Color.Transparent) },
+                            background = { SolidColor(MaterialTheme.colorScheme.inverseOnSurface) },
+                            iconSlot = {
+                                Icon(
+                                    when (it.type) {
+                                        ToastType.Normal,
+                                        ToastType.Info -> Icons.Default.Info
+                                        ToastType.Success -> Icons.Default.Check
+                                        ToastType.Warning -> Icons.Default.Warning
+                                        ToastType.Error -> Icons.Default.Error
                                     },
-                                    iconSlot = {
-                                        Icon(
-                                            when (it.type) {
-                                                ToastType.Normal,
-                                                ToastType.Info -> Icons.Default.Info
-                                                ToastType.Success -> Icons.Default.Check
-                                                ToastType.Warning -> Icons.Default.Warning
-                                                ToastType.Error -> Icons.Default.Error
-                                            },
-                                            null,
-                                            modifier = Modifier.size(24.dp),
-                                            tint = MaterialTheme.colorScheme.inverseSurface,
-                                        )
-                                    },
-                                    messageSlot = {
-                                        val message = it.message as? String ?: return@Toaster
-                                        Text(
-                                            message,
-                                            color = MaterialTheme.colorScheme.inverseSurface,
-                                            fontSize = 16.sp,
-                                            modifier = Modifier.padding(start = 12.dp),
-                                        )
-                                    },
-                                    contentColor = { MaterialTheme.colorScheme.inverseSurface },
-                                    shape = { RoundedCornerShape(8.dp) },
-                                    containerPadding = PaddingValues(48.dp),
+                                    null,
+                                    modifier = Modifier.size(24.dp),
+                                    tint = MaterialTheme.colorScheme.inverseSurface,
                                 )
-                            }
-                        }
+                            },
+                            messageSlot = {
+                                val message = it.message as? String ?: return@Toaster
+                                Text(
+                                    message,
+                                    color = MaterialTheme.colorScheme.inverseSurface,
+                                    fontSize = 16.sp,
+                                    modifier = Modifier.padding(start = 12.dp),
+                                )
+                            },
+                            contentColor = { MaterialTheme.colorScheme.inverseSurface },
+                            shape = { RoundedCornerShape(8.dp) },
+                            containerPadding = PaddingValues(48.dp),
+                        )
                     }
                 }
             }
         }
-    } catch (e: Exception) {
-        Logger.e(e) { "Failed to acquire app lock." }
-        return
     }
 }
