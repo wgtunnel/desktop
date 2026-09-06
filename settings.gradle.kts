5cf77bda790ecfd5e801c6a900857c84070cac09
Zane Schepke <dev@zaneschepke.com>
2026-09-06 04:07:07 -0400

    chore: bump core to 1.5.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,12 +20,12 @@ dependencyResolutionManagement {
 }
 
 // Local dev
-includeBuild("../core") {
-  dependencySubstitution {
-    substitute(module("com.wgtunnel:backend")).using(project(":backend"))
-    substitute(module("com.wgtunnel:parser")).using(project(":parser"))
-  }
-}
+//includeBuild("../core") {
+//  dependencySubstitution {
+//    substitute(module("com.wgtunnel:backend")).using(project(":backend"))
+//    substitute(module("com.wgtunnel:parser")).using(project(":parser"))
+//  }
+//}
 
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
