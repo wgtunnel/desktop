84f3debd91bd50243b2171c35c91c23cebfc4efb
Zane Schepke <dev@zaneschepke.com>
2026-09-14 22:23:58 -0400

    ci: fix chocolatey checksum

--- packaging/chocolatey/tools/chocolateyInstall.ps1
+++ packaging/chocolatey/tools/chocolateyInstall.ps1
@@ -1,21 +1,14 @@
 $ErrorActionPreference = 'Stop'
 $packageName = $env:ChocolateyPackageName
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version     = $env:ChocolateyPackageVersion
 
-$release = Invoke-RestMethod -Uri "https://api.github.com/repos/wgtunnel/desktop/releases/tags/$version" -UserAgent 'wgtunnel-chocolatey'
-$asset = $release.assets | Where-Object { $_.name -like '*.exe' } | Select-Object -First 1
-
-if (-not $asset) {
-  throw "No .exe asset found on GitHub release '$version' for wgtunnel/desktop"
-}
-
-$url64 = $asset.browser_download_url
-
+# URL and checksum are placeholders: publish-chocolatey.yml pins both to the exact release
+# asset at packaging time.
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url64bit       = $url64
+  url64bit       = 'PLACEHOLDER_URL'
+  checksum64     = 'PLACEHOLDER_CHECKSUM'
+  checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'WG Tunnel*'
